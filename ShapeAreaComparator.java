ca14c83695f133175dfdb4b4b285a0a80cbbf43c
ChristopherChow1 <47227923+ChristopherChow1@users.noreply.github.com>
2019-02-26 13:12:39 -0600

    Add files via upload

--- ShapeAreaComparator.java
+++ ShapeAreaComparator.java
@@ -32,6 +32,13 @@ public class ShapeAreaComparator implements Comparator<Shape>
 	@Override
 	public int compare(Shape s1, Shape s2)
 	{
+		if (s1.getArea()>s2.getArea()) {
+			return 1;
+		} else if (s1.getArea()<s2.getArea()) {
+			return -1;
+		} else {
+			return 0;
+		}
 		
 		// TODO: complete this...
 	}
