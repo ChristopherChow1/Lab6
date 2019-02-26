df976c2ca0607283c2001c5c12e0704c6c7a8eae
Christopher Chow <Christopher.Chow-1@ou.edu>
2019-02-26 14:32:48 -0600

    this is my files

---
+++ Rectangle.java
@@ -0,0 +1,27 @@
+
+public class Rectangle extends Shape {
+	private double sideA;
+	private double sideB;
+	public Rectangle(String id, double sideA, double sideB) {
+		super(id);
+		this.sideA = sideA;
+		this.sideB = sideB;
+	}
+	public double getSideA(){
+		return sideA;
+	}
+	public double getSideB() {
+		return sideB;
+	}
+	public double getPerimeter() {
+		double perimeter = 2*(sideA+sideB);
+		return perimeter;
+	}
+	public double getArea() {
+		double area = sideA*sideB;
+		return area;
+	}
+	public String getShapeType() {
+		return "Rectangle";
+	}
+}
---
+++ ShapeSorter.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+/**
+ * Class that aggregates (stores in a list) and sorts shapes.
+ *
+ * Sorting is done either through the natural ordering of shapes
+ * or through a given comparator.
+ *
+ * @author Stephen
+ * @version 2019-02-17
+ */
+public class ShapeSorter
+{
+	/**
+	 * List of shapes to be sorted.
+	 */
+	protected ArrayList<Shape> shapes;
+
+	/**
+	 * Constructor. Initializes member fields.
+	 */
+	public ShapeSorter()
+	{
+		shapes = new ArrayList<Shape>();
+		//sortShapes();
+		
+		// TODO: complete this...
+	}
+
+	/**
+	 * Adds a shape to the list of shapes to be sorted.
+	 *
+	 * @param s Shape to be added.
+	 */
+	public void addShape(Shape s)
+	{
+		shapes.add(s);
+		
+		// TODO: complete this...
+	}
+
+	/**
+	 * Sort the list of shapes by the default ordering (Shapes' compareTo method).
+	 *
+	 * It is recommended that you use Collections.sort().
+	 */
+	public void sortShapes()
+	{
+		Collections.sort(shapes);
+		// TODO: complete this...
+	}
+
+	/**
+	 * Sort the list of shapes by some custom ordering (a class implementing Comparator<Shape>).
+	 *
+	 * It is recommended that you use Collections.sort().
+	 *
+	 * @param comparator The shape comparing object that defines the ordering of the shapes (how to
+	 * sort them).
+	 */
+	public void sortShapes(Comparator<Shape> comparator)
+	{
+		// TODO: complete this...
+		Collections.sort(shapes, comparator);
+	}
+
+	/**
+	 * toString override.
+	 *
+	 * @return The toString of all shared shapes, concatenated. toStrings are added
+	 * in the order that they appear in the list.
+	 */
+	@Override
+	public String toString()
+	{
+		String shapeInfo = "";
+		for (int i = 0; i < shapes.size(); ++i) {
+			shapeInfo += shapes.get(i).toString();
+		}
+		return (shapeInfo);
+		// TODO: complete this...
+	}
+
+}
