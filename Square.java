df8071493cf2d08f58bd6ad8513e59c3c1c9974a
ChristopherChow1 <47227923+ChristopherChow1@users.noreply.github.com>
2019-02-26 13:09:58 -0600

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,21 @@
+
+public class Square extends Rectangle {
+	private double side;
+
+	public Square (String id, double side) {
+		super(id, side, side);
+		this.side = side;
+	}
+	public double getSide() {
+		return side;
+	}
+	public double getPerimeter() {
+		return side*4;
+	}
+	public double getArea() {
+		return side*side;
+	}
+	public String getShapeType() {
+		return "square";
+	}
+}
