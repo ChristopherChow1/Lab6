382d47e44caa9dd42f4725a115c51671794f5866
ChristopherChow1 <47227923+ChristopherChow1@users.noreply.github.com>
2019-02-26 22:51:38 -0600

    Add files via upload

---
+++ Todo.java
@@ -0,0 +1,27 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-02-26
+OU ID_______________: 113403254
+OU EMAIL____________: Christopher.chow-1@ou.edu
+
+Tasks:
+1. finish Circle.java
+2. finish EquilateralTriangle.java
+3. finish rectangle and square.java
+4. finish the sorters
+5. finish the tests
+
+Estimated Time (minutes):
+1. 30
+2. 30
+3. 30
+4. 30
+5. 30
+
+Actual Time (minutes):
+1. 15
+2. 15
+3. 30
+4. 60
+5. 60
+*/}
